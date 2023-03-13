0200e00f1e1e314fe958c2965d118a59bc08c21a
tebanban <estebancalf@gmail.com>
2023-03-13 16:53:38 +0000

    base loades

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import pandas as pd
 from dash import Dash, dcc, html
 
 data = (
-    pd.read_csv("shipping-clean.csv")
-    .query("type == 'conventional' and region == 'Albany'")
-    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
-    .sort_values(by="Date")
+    pd.read_csv("shipping.csv")
+    .query("Source_Area == 'Collect'")
+    .assign(Date=lambda data: pd.to_datetime(data["InvDate"], format="%m/%d/%Y"))
+    .sort_values(by="InvDate")
 )
+
+print(data)
 
 app = Dash(__name__)
