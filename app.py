a06946e80ae86ee3673ea1dc02475c1053ba8986
tebanban <estebancalf@gmail.com>
2023-03-14 20:57:48 +0000

    add scattered graoh

--- app.py
+++ app.py
@@ -5,11 +5,50 @@ from dash import Dash, dcc, html
 
 data = (
     pd.read_csv("shipping.csv")
-    .query("Source_Area == 'Collect'")
-    .assign(Date=lambda data: pd.to_datetime(data["InvDate"], format="%m/%d/%Y"))
-    .sort_values(by="InvDate")
+    .query("type == 'Collect' and R_City =='BENTONVILLE' ")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%m/%d/%Y"))
+    .sort_values(by="Date")
 )
 
 print(data)
 
 app = Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Shipping Analytics"),
+        html.P(
+            children=(
+                "Analyze the behavior of shiping costs"
+                " between years ..."
+            ),
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["neto"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "neto"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Pub"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Pub"},
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
